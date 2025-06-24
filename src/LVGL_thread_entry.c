008467ec955e6f27c5a06c7f80d19e10a4442aab
Jeremy Baker <jeremy.baker@renesas.com>
2025-06-24 12:34:16 +0100

    Update to FSP 6.0.0

--- src/LVGL_thread_entry.c
+++ src/LVGL_thread_entry.c
@@ -1,15 +1,9 @@
 #include <LVGL_thread.h>
 #include "lvgl.h"
 #include "board_init.h"
-#include "lvgl/demos/lv_demos.h"
+#include "demos/lv_demos.h"
 
 
-
-void timer_tick_callback(timer_callback_args_t *p_args)
-{
-    FSP_PARAMETER_NOT_USED(p_args);
-    lv_tick_inc(1);
-}
 
 void vApplicationMallocFailedHook( void )
 {
@@ -21,8 +15,6 @@ void vApplicationMallocFailedHook( void )
 void LVGL_thread_entry(void *pvParameters)
 {
     FSP_PARAMETER_NOT_USED (pvParameters);
-    fsp_err_t err;
-
     lv_init();
 
     board_init();
@@ -48,17 +40,6 @@ void LVGL_thread_entry(void *pvParameters)
     lv_demo_widgets();
 #endif
 
-    err = R_GPT_Open(&g_timer0_ctrl, &g_timer0_cfg);
-    if (FSP_SUCCESS != err)
-    {
-        __BKPT(0);
-    }
-
-    err = R_GPT_Start(&g_timer0_ctrl);
-    if (FSP_SUCCESS != err)
-    {
-        __BKPT(0);
-    }
 
     /* TODO: add your own code here */
     while (1)
--- src/board_init.c
+++ src/board_init.c
@@ -2,7 +2,6 @@
 
 #include "lvgl.h"
 #include "port/lv_port_indev.h"
-#include "lvgl/src/drivers/display/renesas_glcdc/lv_renesas_glcdc.h"
 #include "common_data.h"
 
 
@@ -12,16 +11,12 @@ void board_init(void)
     R_IOPORT_PinWrite(&g_ioport_ctrl, LCD_DISPON, BSP_IO_LEVEL_LOW);
 
 #if 1
-    lv_display_t * disp = lv_renesas_glcdc_direct_create();
-#else
-    /* WARNING
-    Partial render should use a screen size of 1 10th instead of 50th.
-    We are running out of memory, so this is only for testing purposes
-    */
-    static lv_color_t partial_draw_buf[DISPLAY_HSIZE_INPUT0 * DISPLAY_VSIZE_INPUT0 / 50] BSP_PLACE_IN_SECTION(".bss") BSP_ALIGN_VARIABLE(1024);
-
-    lv_display_t * disp = lv_renesas_glcdc_partial_create(partial_draw_buf, NULL, sizeof(partial_draw_buf));
-#endif
+    fsp_err_t err;
+    err = RM_LVGL_PORT_Open(&g_lvgl_port_ctrl, &g_lvgl_port_cfg);
+    if (FSP_SUCCESS != err)
+    {
+        __BKPT(0);
+    }
 
     /* enable the display */
     R_IOPORT_PinWrite(&g_ioport_ctrl, LCD_DISPON, BSP_IO_LEVEL_HIGH);
@@ -29,7 +24,7 @@ void board_init(void)
     /* Enable the backlight */
     R_IOPORT_PinWrite(&g_ioport_ctrl, DISP_BLEN, BSP_IO_LEVEL_HIGH);
 
-    lv_display_set_default(disp);
+    lv_display_set_default(g_lvgl_port_ctrl.p_lv_display);
 
     lv_port_indev_init();
 }
